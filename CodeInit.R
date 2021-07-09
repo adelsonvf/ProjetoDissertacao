abc7a0d856d67a5f23fcbb130d62c795930135cb
adelsonvf <adelson.viegas@hotmail.com>
2021-07-09 16:16:45 -0300

    Arquivos1

--- CodeInit.R
+++ CodeInit.R
@@ -2,15 +2,47 @@ rm(list=ls())#limpar memoria
 cat("\014")#limpar console
 library(data.table)
 library(tidyverse)
+library(plotly)
 df <- fread("DadosBrutos/202101_Transferencias.csv", dec=",",encoding ="Latin-1" )
 df <- df %>% select(c(1,3,4,6,8,16,27,35,36))
-names(df) <- c("AnoMes","Favorecido","Uf","Municipio","Orgao","Programa","PlanoOrcamentario","Favorecido","ValorTransferido")
-dfPM <- df %>% filter(df$Municipio %in% c("PARA DE MINAS", "FLORESTAL", "CONCEICAO DO PARA"))
+names(df) <- c("AnoMes","Tipo","Uf","Municipio","Orgao","Programa","PlanoOrcamentario","Favorecido","ValorTransferido")
+dfPM <- df %>% filter(Municipio %in% c("PARA DE MINAS", "FLORESTAL", "ITAUNA", "CONCEICAO DO PARA"))
 tot <- sum(dfPM$`VALOR TRANSFERIDO`)
 #criar gráficos comparando os valores recebidos no mês de janeiro de 5 cidades da região
 #usar gráficos do ggplot2
-#options(scipen = 999)
-library(ggplot2)
-library(ggalt)
-data("dfPM", package = "ggplot2")
-#Gráfico Inicial
+options(scipen = 999)
+Tot <- dfPM %>% group_by(Municipio) %>% summarise(Total=sum(ValorTransferido))
+  plot1 <- ggplot(data = Tot, aes(x = Municipio,
+                         y = Total,
+                         fill = Municipio,
+                         label = Total)) +
+    geom_col(show.legend = FALSE, position = "dodge") +
+    geom_text(position = position_dodge(width = 0.9), vjust = -0.5) +
+    theme_classic()
+  ggplotly(plot1)
+ ################################################################################
+   plot2 <- ggplot(data = Tot, aes(x = Municipio,
+                         y = Total,
+                         fill = Municipio,
+                         label = Total,
+                         text = paste("Municipio:", Municipio, "<br>",
+                                      "Total:", Total))) +
+    geom_bar(stat = "identity", show.legend = FALSE, position = "dodge") +
+    geom_text(position = position_dodge(width = 0.9), vjust = -0.5) +
+    theme_classic()+
+    coord_flip()
+  ggplotly(plot2, tooltip = "text") %>% layout (showlegend = FALSE)
+################################################################################
+  plot3 <- ggplot(data = Tot, aes(x = Municipio,
+                                  y = Total,
+                                  fill = Municipio,
+                                  label = Total,
+                                  text = paste("Municipio:", Municipio, "<br>",
+                                               "Total:", Total))) +
+    geom_point(stat = "identity", show.legend = FALSE, position = "dodge") +
+    geom_text(position = position_dodge(width = 0.9), vjust = -0.5) +
+    theme_classic()+
+    coord_flip()
+  ggplotly(plot3, tooltip = "text") %>% layout (showlegend = FALSE)
+
+################################################################################
