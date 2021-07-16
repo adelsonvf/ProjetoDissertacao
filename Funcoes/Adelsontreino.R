9d04cb18febc0013d4ed15753b9f7866b24cf573
fsbmat-ufv <fernando.bastos@ufv.br>
2021-07-16 14:06:40 -0300

    update

--- Funcoes/Adelsontreino.R
+++ Funcoes/Adelsontreino.R
@@ -3,6 +3,12 @@ cat("\014")#limpar console
 library(data.table)
 library(tidyverse)
 library(plotly)
-setwd("~/Dissertacao/ProjetoDissertacao/shiny/BancoDados")
-df <- readRDS("df.Rds")
+library(esquisse)
+#setwd("~/Dissertacao/ProjetoDissertacao/shiny/BancoDados")
+df <- readRDS("~/Projetos/3Projetos_de_Pesquisa/2020/ProfMat/Adelson/ProjetoDissertacao/shiny/BancoDados/df.Rds")
 df <- df[1:10000,]
+dfCity <- df %>% group_by(UF) %>% count(MUNICIPIO)
+dfCity %>% ggplot(aes(x = UF, y=count, fill = UF, label = count)) +
+  geom_bar(position = 'dodge', stat='identity', show.legend = F) +
+  geom_text(aes(label=count), position=position_dodge(width=0.9), vjust=-0.25) +
+  theme_minimal()
